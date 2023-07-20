9071a015c71c3b19df70c99d7e0f636694d6797c
Jesús Moreno <j.moreno@abas-bs.com>
2023-07-20 11:45:48 +0200

    initial html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import './css/Variables.css';
+import './css/Reset.css';
+import Layout from './components/layout/Layout';
 
 function App() {
-  return <div className="App">Hola Mandalorians!</div>;
+  return (
+    <Layout>
+      <div className='App'>Hola Mandalorians!</div>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+export function Footer() {
+  return (
+    <footer>
+      <hr />
+      <div className='footer'>
+        <p>@Keepcoding 2023 by Mandalorians</p>
+      </div>
+    </footer>
+  );
+}
