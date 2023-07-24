2277c2af7d1fd64a13c6d94c18f62b7b3544ba99
Jesús Moreno <j.moreno@abas-bs.com>
2023-07-24 19:09:04 +0200

    Idiomas, versión inicial

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,29 @@
 import './Header.css';
+import { useTranslation } from 'react-i18next';
 
 const Header = (...rest) => {
+  const { t, i18n } = useTranslation();
+
   return (
     <header className='header'>
       <nav className='navbar'>
         <h1 className='title'> Header </h1>
+        <div className='icons'>
+          <img
+            className='icon-language'
+            src='/images/languageIcons/es.png'
+            alt='Castellano'
+            title={t('icon-hover')}
+            onClick={() => i18n.changeLanguage('es')}
+          />
+          <img
+            className='icon-language'
+            src='/images/languageIcons/en.png'
+            alt='Inglés'
+            title={t('icon-hover')}
+            onClick={() => i18n.changeLanguage('en')}
+          />
+        </div>
       </nav>
       <hr />
     </header>
