60c5a5fa576134bbbb9792102c464a332053f754
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-07-28 16:46:42 +0200

    Pagination OK

--- src/Components/Adverts/AdvertsListPage.js
+++ src/Components/Adverts/AdvertsListPage.js
@@ -5,6 +5,7 @@ import Advert from './AdvertPage';
 import { connect } from 'react-redux';
 import { getAdverts, getUi } from '../../store/selectors';
 import { advertsList } from '../../store/slices/adverts';
+import '../../stylesheets/advertListPage.css'
 
 
 const EmptyList = ({ dataFiltered }) => {
@@ -14,54 +15,75 @@ const EmptyList = ({ dataFiltered }) => {
         </div>
 };
 
+const advertsPerPage = 2;
+
 const AdvertsListPage = ({ adverts, onAdvertsLoaded, isLoading }) => {
+  //const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const [query, setQuery] = useState('');
+  useEffect(() => {
+    onAdvertsLoaded();
+  }, [onAdvertsLoaded]);
 
-    useEffect(() => {
-        onAdvertsLoaded();
-      }, [onAdvertsLoaded]);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
-      return (
-        <>
-          <div className="container">
-            {isLoading ? (
-              <div className="loadingPage">
-                <div className="loadingInfo">
-                  <h1>LOADING....</h1>
+  const totalPages = Math.ceil(adverts.length / advertsPerPage);
+  const startIndex = (currentPage - 1) * advertsPerPage;
+  const endIndex = startIndex + advertsPerPage;
+  const advertsToDisplay = adverts.slice(startIndex, endIndex);
+
+  return (
+    <>
+      <div className="container">
+        {isLoading ? (
+          <div className="loadingPage">
+            <div className="loadingInfo">
+              <h1>LOADING....</h1>
+            </div>
+          </div>
+        ) : (
+          <div>
+            {!!(adverts && adverts.length) ? (
+              <>
+                <div className="listContainer">
+                  <div className="contaienrTittle">
+                    <h1>ADVERTISEMENTS AVIABLE</h1>
+                  </div>
+                  <ul>
+                    {advertsToDisplay
+                      .sort((a, b) => a.createdAt > b.createdAt)
+                      .map((advert) => (
+                        <li key={advert.id}>
+                          <Link to={`/adverts/${advert.id}`}>
+                            <Advert {...advert} />
+                          </Link>
+                        </li>
+                      ))}
+                  </ul>
                 </div>
-              </div>
+                <div className="pagination">
+                  {[...Array(totalPages)].map((_, index) => (
+                    <button
+                      key={index}
+                      onClick={() => handlePageChange(index + 1)}
+                    >
+                      {index + 1}
+                    </button>
+                  ))}
+                </div>
+              </>
             ) : (
-              <div>
-                {!!(adverts && adverts.length) ? (
-                  <>
-                    <div className="listContainer">
-                      <div className="contaienrTittle">
-                        <h1>ADVERTISEMENTS AVIABLE</h1>
-                      </div>
-                      <ul>
-                        {[...adverts]
-                        .slice()
-                          .sort((a, b) => a.createdAt > b.createdAt)
-                          .map((advert) => (
-                            <li key={advert.id}>
-                              <Link to={`/adverts/${advert.id}`}>
-                                {<Advert {...advert} />}
-                              </Link>
-                            </li>
-                          ))}
-                      </ul>
-                    </div>
-                  </>
-                ) : (
-                  <EmptyList />
-                )}
-              </div>
+              <EmptyList />
             )}
           </div>
-        </>
-      );
-    };
+        )}
+      </div>
+    </>
+  );
+};
+
 
 const mapStateToProps = state => ({
     adverts: getAdverts(state),
