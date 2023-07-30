6e5c1bb5ca39151e7d078fb8cef5dc94d75097bc
Rober <rp_rober@hotmail.com>
2023-07-30 14:16:52 +0200

    test loginPage o.k

--- src/components/auth/_tests_/LoginPage.test.js
+++ src/components/auth/_tests_/LoginPage.test.js
@@ -1,31 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import LoginPage from "../LoginPage";
-
-import { configureStore } from "@reduxjs/toolkit";
-
+import userEvent from "@testing-library/user-event";
+import { authLogin } from "../../../store/slices/auth";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
-import authReducer, { authLogin } from "../../../store/slices/auth";
-import uiReducer, { resetError } from "../../../store/slices/ui";
+import * as defaultState from "../../../store/reducers";
 
-jest.mock("../../../store/slices/auth");
-
-console.log("authred", authReducer);
-console.log("login", authLogin);
+jest.mock("../../../store/slices/auth", () => ({
+  __esModule: true,
+  authLogin: jest.fn(), // Mock only the authLogin action
+}));
 
 describe("LoginPage", () => {
   const renderComponent = (error = null) => {
-    const store = configureStore({
-      reducer: {
-        auth: authReducer,
-        ui: uiReducer, // Agrega el slice uiReducer al store
+    const store = {
+      getState: () => {
+        const state = { ...defaultState }; // Create a copy of the state
+        state.ui.error = error; // Modify the copy instead of the original state
+
+        return state;
       },
-      preloadedState: {
-        auth: false,
-        ui: { error }, // Establece el estado de error en el slice ui
-      },
-    });
+      subscribe: () => {},
+      dispatch: () => {},
+    };
     return render(
       <Provider store={store}>
         <MemoryRouter>
@@ -49,38 +46,38 @@ describe("LoginPage", () => {
 
     //NOTE renderizo el componente
     renderComponent();
-
     const usernameInput = screen.getByLabelText(/Username/);
     const passwordInput = screen.getByLabelText(/Password/);
     const checkboxInput = screen.getByLabelText(/rememberMe/);
     const submitButton = screen.getByRole("button", { name: /Log in/ });
-
     expect(submitButton).toBeDisabled();
 
     //NOTE para lanzar eventos
-
     userEvent.type(usernameInput, credentials.username);
     userEvent.type(passwordInput, credentials.password);
     userEvent.click(checkboxInput, credentials.rememberMe);
 
     expect(submitButton).toBeEnabled();
-
     userEvent.click(submitButton);
-    console.log(authLogin);
+
     expect(authLogin).toHaveBeenCalledWith(credentials);
   });
-
   test("should display an error", () => {
+    //NOTE // Spy on resetError function
+    const resetErrorSpy = jest.spyOn(
+      require("../../../store/slices/ui"),
+      "resetError",
+    );
+
     const error = { message: "Unauthorized" };
+
     renderComponent(error);
-
     const errorElement = screen.getByText(error.message);
+
     expect(errorElement).toBeInTheDocument();
-
     const modalButton = screen.getByTestId("modalButton");
-
     userEvent.click(modalButton);
 
-    expect(resetError).toHaveBeenCalled();
+    expect(resetErrorSpy).toHaveBeenCalled();
   });
 });
