aeaa15ebe04c7e849efa275463f4f36aa42cd6bc
Rober <rp_rober@hotmail.com>
2023-08-11 21:10:28 +0200

    create slice ads y funcion para crear un anuncio

--- src/service/ads.js
+++ src/service/ads.js
@@ -1,3 +1,13 @@
 import client from "../api/client";
 
 const adUrl = "/api/ads/adverts";
+
+export const getAd = adId => {
+  const url = `${adUrl}/${adId}`;
+  return client.get(url);
+};
+
+export const createAd = ad => {
+  const url = `${adUrl}/create`;
+  return client.post(url, ad, {});
+};
---
+++ src/store/slices/ads.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const adsCreate = createAsyncThunk(
+  "ads/create",
+  async (ad, { extra: { service }, rejectWithValue }) => {
+    try {
+      const { id } = await service.ads.createAd(ad);
+      const createdAd = await service.ads.getAd(id);
+      return createdAd;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
+const ads = createSlice({
+  name: "ads",
+  initialState: {
+    areLoaded: false,
+    data: [],
+  },
+  extraReducers: builder => {
+    builder.addCase(adsCreate.fulfilled, (state, action) => {
+      state.data.unshift(action.payload);
+    });
+  },
+});
+
+export default ads.reducer;
