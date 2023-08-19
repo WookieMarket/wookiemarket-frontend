b2c7298ec4d663cc18b31ded80a7bcb79568cdc4
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-08-19 13:51:01 +0200

    AdvertPage Ok

--- src/components/ads/AdvertPage/AdvertPage.js
+++ src/components/ads/AdvertPage/AdvertPage.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import {getAdvertById} from '../../../store/selectors'
+import { getAdvertById } from '../../../store/selectors';
+import Advert from '../Advert/Advert';
 
-const AdvertPage = ({advertId }) => {
+const AdvertPage = ({ advertId }) => {
   const advDate = new Date();
   const { t } = useTranslation();
 
-  const advert = useSelector(state => getAdvertById(state, advertId))
+  const advert = useSelector((state) => getAdvertById(state, advertId));
   return (
     <>
       <div className='content'>
-        <p>Este es el componente AdvertPage</p>
+        <Advert {...advert} />
       </div>
     </>
   );
