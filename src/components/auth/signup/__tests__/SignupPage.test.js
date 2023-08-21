bc003ce0031decb5ad8cba80a3baf9e88a51261e
NJ Skywalker <neylarojas.developer@gmail.com>
2023-08-21 13:19:08 +0200

    User Registration Test
    - Remove unused code from sign up page
    - Add missing translations
    - Adds ui test for SignupPage

---
+++ src/components/auth/signup/__tests__/SignupPage.test.js
@@ -0,0 +1,83 @@
+import { render, screen } from '@testing-library/react';
+import SignupPage from '../SignupPage';
+import userEvent from '@testing-library/user-event';
+import { authSignup } from '../../../../store/slices/auth';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import * as defaultState from '../../../../store/reducers';
+
+jest.mock('../../../../store/slices/auth', () => ({
+  __esModule: true,
+  authSignup: jest.fn(),
+}));
+
+describe('Signup', () => {
+  const renderComponent = (error = null) => {
+    const store = {
+      getState: () => {
+        const state = { ...defaultState };
+        state.ui.error = error;
+        return state;
+      },
+      subscribe: () => {},
+      dispatch: () => {},
+    };
+    return render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <SignupPage />
+        </MemoryRouter>
+      </Provider>
+    );
+  };
+
+  test('snapshot', () => {
+    const { container } = renderComponent();
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should dispatch signup action', () => {
+    const userData = {
+      username: 'user123',
+      password: 'supersegurisimo',
+      email: 'user123@test.com',
+    };
+
+    // Render component
+    renderComponent();
+
+    // Test UI components
+    const usernameInput = screen.getByLabelText(/Username/);
+    const passwordInput = screen.getByLabelText(/Password/);
+    const emailInput = screen.getByLabelText(/Email/);
+    const submitButton = screen.getByRole('button', { name: /Register/ });
+    expect(submitButton).toBeDisabled();
+
+    // Test button after fulfilling text fields
+    userEvent.type(usernameInput, userData.username);
+    userEvent.type(passwordInput, userData.password);
+    userEvent.type(emailInput, userData.email);
+    expect(submitButton).toBeEnabled();
+    userEvent.click(submitButton);
+
+    expect(authSignup).toHaveBeenCalledWith(userData);
+  });
+
+  test('should display an error', () => {
+    const resetErrorSpy = jest.spyOn(
+      require('../../../../store/slices/ui'),
+      'resetError'
+    );
+
+    const error = { message: 'Bad request' };
+
+    renderComponent(error);
+    const errorElement = screen.getByText(error.message);
+
+    expect(errorElement).toBeInTheDocument();
+    const modalButton = screen.getByTestId('modalButton');
+    userEvent.click(modalButton);
+
+    expect(resetErrorSpy).toHaveBeenCalled();
+  });
+});
