97a1771c0cbf2b52819c5d28af0f181365d7b1b6
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-08-22 14:14:13 +0200

    selector for categories OK

--- src/service/ads.js
+++ src/service/ads.js
@@ -1,26 +1,26 @@
-import client from "../api/client";
+import client from '../api/client';
 
-const adUrl = "/api/ads/adverts";
+const adUrl = '/api/ads/adverts';
 
-export const getAd = adId => {
+export const getCategories = (adCategories) => {
+  const url = `${adUrl}/categories`;
+  return client.get(url);
+};
+
+export const getAd = (adId) => {
   const url = `${adUrl}/${adId}`;
   return client.get(url);
 };
 
-export const createAd = ad => {
+export const createAd = (ad) => {
   const config = {
     headers: {
-      "Content-Type": "multipart/form-data",
+      'Content-Type': 'multipart/form-data',
     },
   };
   const url = `${adUrl}/create`;
   return client.post(url, ad, config);
 };
-
-// export const getLastAdv = () => {
-//   const url = `${adUrl}`;
-//   return client.get(url);
-// };
 
 export const getRecentAds = () => {
   const url = `${adUrl}`;
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,7 +1,8 @@
-export const getIsLogged = state => state.auth;
-export const areAdvertsLoaded = state => state.ads.areLoaded;
-export const getAdverts = state => (state.ads.data ? state.ads.data : []);
-export const getAdsPerPage = state => state.ads.adsPerPage;
+export const getIsLogged = (state) => state.auth;
+export const areAdvertsLoaded = (state) => state.ads.areLoaded;
+export const getAdverts = (state) => (state.ads.data ? state.ads.data : []);
+export const getAdsPerPage = (state) => state.ads.adsPerPage;
+export const getCategoriesList = (state) =>
+  state.ads.categories ? state.ads.categories : [];
 
-
-export const getUi = state => state.ui;
+export const getUi = (state) => state.ui;
