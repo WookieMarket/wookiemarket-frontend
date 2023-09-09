6a9b611ed4d9e568e788e870b16e4a8211220caf
Rober <rp_rober@hotmail.com>
2023-09-09 12:50:52 +0200

    FavoriteAds o.k

--- src/components/shared/FavoriteAds/FavoriteAds.js
+++ src/components/shared/FavoriteAds/FavoriteAds.js
@@ -7,7 +7,6 @@ import './FavoriteAds.css';
 import estrella from '../../../assets/CSS-Images/estrella.png';
 
 function FavoriteAds({ id }) {
-  // Usa el selector isFavorite para obtener el estado de favoritos
   const isAdFavorite = useSelector(isFavorite(id));
   const dispatch = useDispatch();
 
@@ -19,15 +18,11 @@ function FavoriteAds({ id }) {
     }
   };
 
-  //const buttonClass = isAdFavorite ? 'favorite-button' : 'not-favorite-button';
+  const buttonClass = isAdFavorite ? 'favorite-button' : 'not-favorite-button';
 
   return (
-    <button onClick={toggleFavorite}>
-      {isAdFavorite ? (
-        <img className="favorite-button" src={estrella} alt="estrella" />
-      ) : (
-        <FaRegStar className="not-favorite-button" />
-      )}
+    <button className={buttonClass} onClick={toggleFavorite}>
+      {isAdFavorite ? <img src={estrella} alt="estrella" /> : <FaRegStar />}
     </button>
   );
 }
