807fe4609df44c94187e538b0d51428f18b8a641
Rober <rp_rober@hotmail.com>
2023-09-15 15:57:48 +0200

    errores si no hay conexion

--- src/components/ads/AdvertsListPage/FavoriteAdsList.js
+++ src/components/ads/AdvertsListPage/FavoriteAdsList.js
@@ -1,16 +1,35 @@
 import AdsList from '../AdsList/AdsList';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getFavoriteAds } from '../../../store/selectors';
+import { getFavoriteAds, getUi } from '../../../store/selectors';
 import { getFavorite } from '../../../store/slices/ads';
+import { resetError } from '../../../store/slices/ui';
+import ErrorModal from '../../shared/modal/ErrorModal';
 
 const FavoriteAdsList = () => {
+  const { error } = useSelector(getUi);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFavorite()).catch(error => console.log(error));
   }, [dispatch]);
 
-  return <AdsList selector={getFavoriteAds} />;
+  const handleErrorClick = () => {
+    dispatch(resetError());
+  };
+
+  return (
+    <div>
+      <AdsList selector={getFavoriteAds} />;
+      {error && (
+        <ErrorModal
+          buttonErrorId="errorFavoriteAds"
+          title="Error"
+          message={error.message}
+          onCancel={handleErrorClick}
+        />
+      )}
+    </div>
+  );
 };
 
 export default FavoriteAdsList;
